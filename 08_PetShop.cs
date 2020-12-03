d8b5412835c1a223c3c645a37220a565e3408db1
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-03 16:20:05 +0200

    First-Steps-In-Coding

---
+++ 08_PetShop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08_PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsCount = int.Parse(Console.ReadLine());
+            int animalsCount = int.Parse(Console.ReadLine());
+
+            double foodPriceForDogs = dogsCount * 2.50;
+            double foodPriceForAnimals = animalsCount * 4;
+
+            double totalSum = foodPriceForDogs + foodPriceForAnimals;
+
+            Console.WriteLine($"{totalSum:f1} lv.");
+        }
+    }
+}
