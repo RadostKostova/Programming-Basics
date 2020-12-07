f50aab783f705c1cbf06ad8d66ff0ff669897445
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-08 01:35:35 +0200

    Programming-Basics

---
+++ FirstStepsInCodingExercise/02_RadiansToDegrees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double result = radians * (180 / Math.PI);
+
+            Console.WriteLine(Math.Round(result, 2));
+        }
+    }
+}
