41575e74aa4a129b9d4e7b661c21ce87bc465187
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-09 08:52:50 +0200

    Programming-Basics

---
+++ FirstStepsInCodingExercise/VacationBooksList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04_VacantionBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            double pagesReadPErHour = double.Parse(Console.ReadLine());
+            int daysToRead = int.Parse(Console.ReadLine());
+
+            double totalHours = pages / pagesReadPErHour;
+            double hoursPerDay = totalHours / daysToRead;
+
+            Console.WriteLine($"{hoursPerDay:f1}");
+
+        }
+    }
+}
