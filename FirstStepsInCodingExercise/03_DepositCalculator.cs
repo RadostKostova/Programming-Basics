922ab4dd2827371bc13a59e3c5e3265977e1afac
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-09 08:51:43 +0200

    Programming-Basics

---
+++ FirstStepsInCodingExercise/03_DepositCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03_DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double interest = deposit * percent;
+            double interest1 = interest / 12;
+            double sum = deposit + (3 * (months) * interest1);
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
