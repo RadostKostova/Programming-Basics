f6a21b2d69ff8bcb5dcbed6ce297b7bf130cc54e
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-11 12:54:13 +0200

    Add files via upload

---
+++ FirstStepsInCodingExercise/07_FruitMarket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double strawberriesPrice = double.Parse(Console.ReadLine());
+			double bananasquantity = double.Parse(Console.ReadLine());
+			double orangesquantity = double.Parse(Console.ReadLine());
+			double raspberriesquantity = double.Parse(Console.ReadLine());
+			double strawberriesquantity = double.Parse(Console.ReadLine());
+			
+			double raspberriesPrice = strawberriesPrice / 2;
+			double orangesPrice = raspberriesPrice - 0.4 * raspberriesPrice;
+			double bananasPrice = raspberriesPrice - 0.8 * raspberriesPrice;
+
+			double bananasSum = bananasquantity * bananasPrice;
+			double orangesSum = orangesquantity * orangesPrice;
+			double raspberriesSum = raspberriesquantity * raspberriesPrice;
+			double strawberriesSum = strawberriesquantity * strawberriesPrice;
+
+			double totalSum = (raspberriesSum + orangesSum + bananasSum + strawberriesSum);
+			Console.WriteLine(Math.Round(totalSum, 2));
+		}
+    }
+}
