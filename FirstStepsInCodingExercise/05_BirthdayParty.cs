afc39ee13bd26d26a6c72a4adbf9e5e0fb7a0eb1
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-13 17:25:42 +0200

    Programming-Basics

---
+++ FirstStepsInCodingExercise/05_BirthdayParty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05_Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallPrice = int.Parse(Console.ReadLine());
+
+            double cakePrice = hallPrice * 0.2;
+            double drinksPrice = cakePrice - cakePrice * 0.45;
+            double animator = hallPrice / 3;
+
+            double totalPrice = hallPrice + cakePrice + drinksPrice + animator;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
