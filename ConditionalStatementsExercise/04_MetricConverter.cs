5dfaa990a2823daae91d0a17049b52a5b0cad9b7
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-19 12:06:15 +0200

    Programming-Basics

---
+++ ConditionalStatementsExercise/04_MetricConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04_MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string output = Console.ReadLine();
+
+            switch (input)
+            {
+                case "m": number /= 1; break;
+                case "mm": number /= 1000; break;
+                case "cm": number /= 100; break;
+                case "inch": number /= 39.37; break;
+                default:
+                    break;
+            }
+
+            switch (output)
+            {
+                case "m": number *= 1; break;
+                case "mm": number *= 1000; break;
+                case "cm": number *= 100; break;
+                case "inch": number *= 39.37; break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"{number:f3}");
+        }
+    }
+}
