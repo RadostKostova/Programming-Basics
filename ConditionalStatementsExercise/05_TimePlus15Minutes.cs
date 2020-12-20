d62f4bf07dcb43b86e44eaa8148463c5cb5f29fd
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-20 16:59:15 +0200

    Add files via upload

---
+++ ConditionalStatementsExercise/05_TimePlus15Minutes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05_TimePlus15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int sum = ((hour * 60) + minutes + 15);
+            int resulthour = sum / 60;
+
+            if (resulthour >= 24)
+            {
+                resulthour -= 24;
+            }
+
+            int resultmin = sum % 60;
+
+            Console.WriteLine("{0}:{1:00}", resulthour, resultmin);
+        }
+    }
+}
