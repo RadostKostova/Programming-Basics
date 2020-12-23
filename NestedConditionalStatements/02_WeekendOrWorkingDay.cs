31b3f1d5a29482c45fd49eaedb6f88dff9eee55e
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-23 13:25:56 +0200

    Programming-Basics

---
+++ NestedConditionalStatements/02_WeekendOrWorkingDay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_WeekendOrWorkingDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Monday":
+                case "Thuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine("Weedend");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
