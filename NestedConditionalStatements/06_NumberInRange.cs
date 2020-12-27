5d4f01eae4a2d5a2facb0cb071fcea6b755aa72a
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-27 18:28:56 +0200

    Programming-Basics

---
+++ NestedConditionalStatements/06_NumberInRange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n != 0 && n <= 100 && n >= - 100 )
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
