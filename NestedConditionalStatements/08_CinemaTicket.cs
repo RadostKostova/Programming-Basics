a097eebfb344a53940c14f1e5504e3e704cc047b
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-29 11:52:55 +0200

    Programming-Basics

---
+++ NestedConditionalStatements/08_CinemaTicket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _08_CinemaTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            int ticketPrice = 0;
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                    ticketPrice = 12;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Tuesday":
+                    ticketPrice = 12;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Wednesday":
+                    ticketPrice = 14;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Thursday":
+                    ticketPrice = 14;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Friday":
+                    ticketPrice = 12;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Saturday":
+                    ticketPrice = 16;
+                    Console.WriteLine(ticketPrice);
+                    break;
+                case "Sunday":
+                    ticketPrice = 16;
+                    Console.WriteLine(ticketPrice);
+                    break;
+            }
+        }
+    }
+}
