29dda62eec6604408d5f9ce9a9ba0eab6ab35fe8
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2020-12-31 13:22:17 +0200

    Programming-Basics

---
+++ NestedConditionalStatements/10_InvalidNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10_InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 0 || num >= 100 && num <= 200)
+            {
+                Console.WriteLine(); // should print nothing
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
