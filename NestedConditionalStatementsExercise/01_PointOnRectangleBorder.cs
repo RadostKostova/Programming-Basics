5e034c5a76b5b9c5e32ae0be791a3bd3e510b158
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-03 11:26:17 +0200

    Programming-Basics

---
+++ NestedConditionalStatementsExercise/01_PointOnRectangleBorder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01_PointOnRectangleBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine()); //rectangle parameters
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            int x = int.Parse(Console.ReadLine());  //point parameters
+            int y = int.Parse(Console.ReadLine());
+
+            if ((x > x1 && x < x2 && y > y1 && y < y2) || ((x < x1 || y < y1 || x > x2 || y > y2)))
+            {
+                Console.WriteLine("Inside / Outside");
+            }
+            else
+            {
+                Console.WriteLine("Border");
+            }
+        }
+    }
+}
