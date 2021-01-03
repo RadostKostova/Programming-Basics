12ff08b9956e0a725568c4049bf56decf9990c46
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-03 11:27:26 +0200

    Programming-Basics

---
+++ NestedConditionalStatementsExercise/02_Cinema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double priceTicket = 0;
+
+            switch (projectType)
+            {
+                case "Premiere":
+                    priceTicket = 12;
+                    break;
+                case "Normal":
+                    priceTicket = 7.5;
+                    break;
+                case "Discount":
+                    priceTicket = 5;
+                    break;
+            }
+
+            double totalIncome = priceTicket * rows * cols;
+
+            Console.WriteLine($"{totalIncome:f2} leva");
+        }
+    }
+}
