616ad8bc8b5c0190359bccbbb64156440e26ab07
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-06 11:51:51 +0200

    Programming-Basics

---
+++ NestedConditionalStatementsExercise/06_Journey.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _06_Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string place = "";
+
+            double result = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                switch (season)
+                {
+                    case "winter":
+                        result = budget * 0.7;
+                        place = "Hotel";
+                        break;
+                    case "summer":
+                        result = budget * 0.3;
+                        place = "Camp";
+                        break;
+                }
+            }
+            else if (budget <= 1000 && budget > 100)
+            {
+                destination = "Balkans";
+                switch (season)
+                {
+                    case "winter":
+                        result = budget * 0.8;
+                        place = "Hotel";
+                        break;
+                    case "summer":
+                        result = budget * 0.4;
+                        place = "Camp";
+                        break;
+                }
+            }
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+                place = "Hotel";
+                result = budget * 0.9;
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{place} - {result:F2}");
+        }
+    }
+}
