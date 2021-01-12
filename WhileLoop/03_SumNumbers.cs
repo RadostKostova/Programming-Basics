3030dadc97d93f8dfc04aa0ccd26b5a641a8a4f8
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-12 10:49:07 +0200

    Programming-Basics

---
+++ WhileLoop/03_SumNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03_SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = 0;
+            int min = 0;
+
+            while (input != "END")
+            {
+                int n = int.Parse(input);
+
+                if (n > max)
+                {
+                    max = n;
+                }
+
+                if (n < min)
+                {
+                    min = n;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
