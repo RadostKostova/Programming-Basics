de88579e78d6fb0db5fc489d0ae07853f50b25f5
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-14 10:34:15 +0200

    Programming-Basics

---
+++ WhileLoop/05_AccountBalance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05_AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int entries = int.Parse(Console.ReadLine());
+                int counter = 1;
+                double currentSum = 0;
+
+                while (counter <= entries)
+                {
+                    double income = double.Parse(Console.ReadLine());
+                    if (income < 0)
+                    {
+                        Console.WriteLine("Invalid operation!");
+                        break;
+                    }
+                    else
+                    {
+                        currentSum += income;
+                        Console.WriteLine($"Your account balance was increased by: {income:f2}");
+                    }
+                    counter++;
+                }
+                Console.WriteLine($"Total balance: {currentSum}");
+            }
+    }
+}
