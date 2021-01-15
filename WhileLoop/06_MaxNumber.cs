77f212eadd7614c371113b3640e1d5d5cac01495
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-15 11:12:57 +0200

    Programming-Basics

---
+++ WhileLoop/06_MaxNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06_MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
