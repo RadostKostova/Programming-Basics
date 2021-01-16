5cff6acc983b1f89e77474c6429f2b0c59253798
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-16 11:05:46 +0200

    Programming-Basics

---
+++ WhileLoop/07_MinNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07_MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber > minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(minNumber);
+        }
+    }
+}
