efe50145a37a892a7b6b6cceb3a26dc40f47e229
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-18 12:24:43 +0200

    Programming-Basics

---
+++ WhileLoop/09_GraduationPt2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09_GraduationPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 1;
+            double sum = 0;
+            int poorGrades = 0;
+
+            while (grade <= 12)
+            {
+                double grades = double.Parse(Console.ReadLine());
+
+                if (grades >= 4)
+                {
+                    sum += grades;
+                    grades++;
+                }
+                else
+                {
+                    poorGrades += 1;
+                }
+
+                if (poorGrades > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {grades} grade"); break;
+                }
+            }
+            if (poorGrades < 2)
+            {
+                double average = sum / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {average:F2}");
+            }
+        }
+    }
+}
