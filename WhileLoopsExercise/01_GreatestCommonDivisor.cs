f6b59857564990064d1efd7d0b0df1baa8b88916
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-20 12:56:03 +0200

    Programming-Basics

---
+++ WhileLoopsExercise/01_GreatestCommonDivisor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01_GreatestCommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int gcd = 0;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
+
+            if (a == 0)
+            {
+                gcd = b;
+            }
+            else
+            {
+                gcd = a;
+            }
+
+            Console.WriteLine(gcd);
+        }
+    }
+}
