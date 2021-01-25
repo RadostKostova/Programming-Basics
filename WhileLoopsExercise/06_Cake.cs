a8d545a01d6caabcb3f462ed16cc1c350bd8e095
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-25 10:14:18 +0200

    Programming-Basics

---
+++ WhileLoopsExercise/06_Cake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06_Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenthCake = int.Parse(Console.ReadLine());
+            int widthCake = int.Parse(Console.ReadLine());
+            int partsCake = lenthCake * widthCake;
+
+            for (int i = 0; i < 1000000; i++) //with for loop
+            {
+                string partsC = Console.ReadLine();
+
+                if (partsC == "STOP")
+                {
+                    Console.WriteLine("{0} pieces are left.", Math.Abs(partsCake));
+                    break;
+                }
+
+                int parts = int.Parse(partsC);
+                partsCake = partsCake - parts;
+
+                if (partsCake < 0)
+                {
+                    Console.WriteLine("No more cake left! You need {0} pieces more.", Math.Abs(partsCake)); break;
+                }
+            }
+        }
+    }
+}
