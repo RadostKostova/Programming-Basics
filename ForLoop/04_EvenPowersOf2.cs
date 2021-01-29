680c3b614d13e591463f78ce1fcd48b116a751aa
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-01-29 15:14:17 +0200

    Programming-Basics

---
+++ ForLoop/04_EvenPowersOf2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04_EvenPowersOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for (var i = 0; i <= n; i += 2)
+            {
+                Console.WriteLine(num);
+                num = num * 2 * 2;
+            }
+        }
+    }
+}
