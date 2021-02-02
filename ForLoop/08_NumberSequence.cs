66cf2f147060b4980e0e8df0461d8f05d7151e48
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-02-02 13:06:50 +0200

    Programming-Basics

---
+++ ForLoop/08_NumberSequence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08_NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
