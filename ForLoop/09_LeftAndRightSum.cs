e5d3eee1adb3918881a4a02d73812be9b91d3ed3
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-02-03 11:46:20 +0200

    Programming-Basics

---
+++ ForLoop/09_LeftAndRightSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
