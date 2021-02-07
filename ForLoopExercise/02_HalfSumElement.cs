40873c822807f96bc4f33b33fe887beb3396e778
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-02-07 09:48:52 +0200

    Programming-Basics

---
+++ ForLoopExercise/02_HalfSumElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02_HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int big = int.Parse(Console.ReadLine());
+            int sum = big;
+
+            for (var i = 2; i <= n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                sum += a;
+
+                if (a > big)
+                {
+                    big = a;
+                }
+            }
+
+            if (big == (sum - big))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum  = " + big);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(big - (sum - big)));
+            }
+        }
+    }
+}
