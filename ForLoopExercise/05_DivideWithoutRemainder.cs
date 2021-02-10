539d42852873c94f14caf609811e31ae3cc104bb
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-02-10 10:21:42 +0200

    Programming-Basics

---
+++ ForLoopExercise/05_DivideWithoutRemainder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05_DivideWithoutReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int numb = int.Parse(Console.ReadLine());
+                if (numb % 2 == 0)
+                {
+                    count++;
+                    p1 = count * 1.00 / 10 * 100;
+                }
+                if (numb % 3 == 0)
+                {
+                    count++;
+                    p2 = count * 1.00 / 10 * 100;
+                }
+                if (numb % 4 == 0)
+                {
+                    count++;
+                    p3 = count * 1.00 / 10 * 100;
+                }
+            }
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+        }
+    }
+}
