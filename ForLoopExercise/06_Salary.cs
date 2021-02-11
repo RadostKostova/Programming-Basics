fa259f224fabeb5c570fdd2dad7d84518984c8e4
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-02-11 14:29:55 +0200

    Programming-Basics

---
+++ ForLoopExercise/06_Salary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _06_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openedTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < openedTabs; i++)
+            {
+                string websiteName = Console.ReadLine();
+
+                if (websiteName == "Facebook")
+                {
+                    salary -= 150;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        return;
+                    }
+                }
+                else if (websiteName == "Instagram")
+                {
+                    salary -= 100;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        return;
+                    }
+                }
+                else if (websiteName == "Reddit")
+                {
+                    salary -= 50;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(salary);
+        }
+    }
+}
