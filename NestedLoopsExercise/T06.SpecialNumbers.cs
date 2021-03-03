58525f5ed145b715b1be19f733d7bceea758b037
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-03 19:37:45 +0200

    Programming-Basics

---
+++ NestedLoopsExercise/T06.SpecialNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T06.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool spec = true;
+            int a;
+            int b = 1;
+
+            for (int i = 1111; i < 10000; i++)
+            {
+                b = i;
+
+                while (b != 0)
+                {
+                    a = b % 10;
+
+                    if (a == 0)
+                    {
+                        spec = false;
+                        break;
+                    }
+                    else if (n % a == 0)
+                    { 
+                        b = b / 10; 
+                    }
+                    else
+                    {
+                        spec = false;
+                        break;
+                    }
+
+                    spec = true;
+                }
+
+                if (spec == true) 
+                { 
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
